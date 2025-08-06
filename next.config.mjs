93a901dbe1ababdb403d9d7cf45d9bf665b71791
raktimproloy <raktimproloy@gmail.com>
2025-08-06 23:23:44 +0600

    add next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+const config = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+export default config;
